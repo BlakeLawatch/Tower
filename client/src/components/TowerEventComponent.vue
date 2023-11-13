bf1fade15f9e73cd24a3e66c9091df9224452774
Blake Lawatch <blakelawatch@gmail.com>
2023-11-13 14:33:30 -0700

    Account page in works

--- client/src/components/TowerEventComponent.vue
+++ client/src/components/TowerEventComponent.vue
@@ -1,12 +1,14 @@
 <template>
-    <router-link :to="{name: 'EventDetails', params: { eventId: towerEventProp.id}}">
+    <router-link :to="{ name: 'EventDetails', params: { eventId: towerEventProp.id } }">
         <div class="rounded text-center">
-            <img v-if="towerEventProp.isCanceled" class="rounded my-3 img-cancelled" :src="towerEventProp.coverImg" alt="event in your area">
+            <img v-if="towerEventProp.isCanceled" class="rounded my-3 img-cancelled" :src="towerEventProp.coverImg"
+                alt="event in your area">
             <img v-else class="rounded my-3" :src="towerEventProp.coverImg" alt="event in your area">
 
-           <p v-if="towerEventProp.isCanceled"  class="text-center rounded bg-dark text-light p-1 my-1">Event has been cancelled</p>
-           <p v-else class="text-center rounded bg-dark text-light p-1 my-1">{{ towerEventProp.name }}</p>
-           </div>
+            <p v-if="towerEventProp.isCanceled" class="text-center rounded bg-dark text-light p-1 my-1">Event has been
+                cancelled</p>
+            <p v-else class="text-center rounded bg-dark text-light p-1 my-1">{{ towerEventProp.name }}</p>
+        </div>
     </router-link>
 </template>
 
@@ -16,26 +18,25 @@ import { TowerEvent } from '../models/TowerEvent';
 
 
 export default {
-    props: { towerEventProp: {type: TowerEvent, required: true}},
-    setup(){
-    return { 
-        
-     }
+    props: { towerEventProp: { type: TowerEvent, required: true } },
+    setup() {
+        return {
+
+        }
     }
 };
 </script>
 
 
 <style lang="scss" scoped>
-
-img{
+img {
     height: 20vh;
     width: 20vw;
     object-fit: cover;
     object-position: center;
 }
 
-.img-cancelled{
+.img-cancelled {
     border: 2px solid red;
     opacity: 0.5;
 }
